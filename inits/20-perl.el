1b468f3707adb6a355e20e8e2602da8005b36d10
aereal <aereal@aereal.org>
2014-04-24 12:48:00 +0900

    Add smartchr configurations for Perl
    ref. #1

--- inits/20-perl.el
+++ inits/20-perl.el
@@ -1,1 +1,9 @@
 (defalias 'perl-mode 'cperl-mode)
+
+(defun my/perl-smartchr ()
+  (interactive)
+  (define-key cperl-mode-map (kbd ".") (smartchr '("." "->")))
+  (define-key cperl-mode-map (kbd ",") (smartchr '(", " " => " ",")))
+  (define-key cperl-mode-map (kbd "~") (smartchr '(" =~ " " !~ " " ~~ " "~"))))
+
+(add-hook 'cperl-mode-hook 'my/perl-smartchr)
